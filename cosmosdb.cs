fbe8679a3dd6b6f375051b48b5270fc6bc0fd4f9
Bjørn Atle Vorland <bvorland@microsoft.com>
2022-06-28 14:35:33 +0200

    push it

---
+++ cosmosdb.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace function32717
+{
+    public static class cosmosdb
+    {
+        [FunctionName("cosmosdb")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "SampleDB",
+            collectionName: "SampleContainer",
+            ConnectionStringSetting = "CosmosDBConnection",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
+            ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                log.LogInformation("First document Id " + input[0].Id);
+            }
+        }
+    }
+}
